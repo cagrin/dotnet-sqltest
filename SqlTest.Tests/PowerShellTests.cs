556f5cdf0d2ffe694b68568434326db65537a04b
Krzysztof Strugiński <cagrin@gmail.com>
2022-04-10 00:52:17 +0200

    Add introducrion to Runtimes

--- SqlTest.Tests/PowerShellTests.cs
+++ SqlTest.Tests/PowerShellTests.cs
@@ -1,6 +1,7 @@
 namespace SqlTest.Tests;
 
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using LikeComparison.TransactSql;
 
 [TestClass]
@@ -18,5 +19,47 @@ public class PowerShellTests
         var psout = psobjs.First().BaseObject.ToString();
 
         Assert.IsTrue(psout?.Like("6.%"));
+    }
+
+    [TestMethod]
+    public void RunDockerScript()
+    {
+        string script = @"
+        docker container stop test_image
+        docker container rm test_image
+        ";
+
+        RunScript(script);
+    }
+
+    private static void RunScript(string script)
+    {
+        var runspace = RunspaceFactory.CreateRunspace(InitialSessionState.CreateDefault());
+        runspace.Open();
+
+        using var ps = PowerShell.Create();
+        _ = ps.AddScript(script);
+        ps.Runspace = runspace;
+
+        try
+        {
+            var results = ps.Invoke();
+
+            foreach (var result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            foreach (var error in ps.Streams.Error)
+            {
+                Console.WriteLine(error);
+            }
+        }
+        catch (RuntimeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        runspace.Close();
     }
 }
