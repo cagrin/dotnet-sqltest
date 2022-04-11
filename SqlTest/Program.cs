22b9c4cea40327ee67fae76271a62d329011d8ed
Krzysztof Strugiński <cagrin@gmail.com>
2022-04-11 19:59:50 +0200

    Add introduction to System.CommandLine

--- SqlTest/Program.cs
+++ SqlTest/Program.cs
@@ -1,6 +1,6 @@
 namespace SqlTest;
 
-using System.Reflection;
+using System.CommandLine;
 
 public static class Program
 {
@@ -11,18 +11,8 @@ public static class Program
             throw new ArgumentNullException(nameof(args), "Value cannot be null.");
         }
 
-        if (args.Length == 0)
-        {
-            var ea = Assembly.GetEntryAssembly();
-            if (ea != null)
-            {
-                var aiva = ea.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-                if (aiva != null)
-                {
-                    var versionString = aiva.InformationalVersion.ToString().Split("+").First();
-                    Console.WriteLine($"sqltest v{versionString}\n------------\nUsage: sqltest [options]");
-                }
-            }
-        }
+        var rootCommand = new RootCommand() { Description = "Command line tool for running tSQLt unit tests from MSBuild.Sdk.SqlProj projects." };
+
+        _ = rootCommand.Invoke(args);
     }
 }
