14724339acddb680396822559929742f13942fc8
Krzysztof Strugiński <cagrin@gmail.com>
2022-04-13 23:51:21 +0200

    Add execute tSQLt.RunAll

--- SqlTest/RunAllCommand.cs
+++ SqlTest/RunAllCommand.cs
@@ -1,7 +1,9 @@
 namespace SqlTest;
 
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Management.Automation;
+using Dapper;
 using DotNet.Testcontainers.Containers.Builders;
 using DotNet.Testcontainers.Containers.Configurations.Databases;
 using DotNet.Testcontainers.Containers.Modules.Databases;
@@ -15,21 +17,27 @@ public class RunAllCommand : IDisposable
         var password = "A.794613";
 
         var testcontainersBuilder = new TestcontainersBuilder<MsSqlTestcontainer>()
-            .WithDatabase(new MsSqlTestcontainerConfiguration()
+            .WithDatabase(new MsSqlTestcontainerConfiguration(image)
             {
                 Password = password,
-            })
-            .WithImage(image);
+            });
 
         this.testcontainer = testcontainersBuilder.Build();
         this.testcontainer.StartAsync().Wait();
 
         int port = this.testcontainer.Port;
         password = this.testcontainer.Password;
+        string cs = this.testcontainer.ConnectionString;
 
         string script = $"dotnet publish {project} /p:TargetServerName=localhost /p:TargetPort={port} /p:TargetDatabaseName=Database.Tests /p:TargetUser=sa /p:TargetPassword={password}";
 
-        return PowerShellCommand.Invoke(script);
+        var result = PowerShellCommand.Invoke(script);
+
+        using var con = new SqlConnection($"{cs}TrustServerCertificate=True;");
+
+        _ = con.Execute("EXEC [Database.Tests].tSQLt.RunAll");
+
+        return result;
     }
 
     public void Dispose()
