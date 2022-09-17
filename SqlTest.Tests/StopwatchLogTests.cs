75afa717a37cb8ad7ebe03bb5c7ff2163784cb6e
Krzysztof Strugiński <cagrin@gmail.com>
2022-09-17 22:56:00 +0200

    Add ShouldRunAboveSecond test

--- SqlTest.Tests/StopwatchLogTests.cs
+++ SqlTest.Tests/StopwatchLogTests.cs
@@ -6,12 +6,24 @@ using SqlTest;
 public class StopwatchLogTests
 {
     [TestMethod]
-    public void ShoudRunBelowMilisecond()
+    public void ShouldRunBelowMilisecond()
     {
         var mock = new MockIConsole();
 
         new StopwatchLog(mock.Object).Start("Something...").Stop();
 
         Assert.That.IsLike(mock.Output, "Something... _ ms");
+    }
+
+    [TestMethod]
+    public void ShouldRunAboveSecond()
+    {
+        var mock = new MockIConsole();
+
+        var watch = new StopwatchLog(mock.Object).Start("Something...");
+        Thread.Sleep(1000);
+        watch.Stop();
+
+        Assert.That.IsLike(mock.Output, "Something... _ s");
     }
 }
