46efe605b8ab85c07ef6a8526b26084ac31b16ac
Krzysztof Strugiński <80916480+cagrin@users.noreply.github.com>
2023-09-12 11:52:55 +0000

    Update devcontainers features

--- .devcontainer/dotnet-install/install.sh
+++
@@ -1,2 +0,0 @@
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 7.0 --install-dir /usr/share/dotnet
-curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0 --install-dir /usr/share/dotnet
