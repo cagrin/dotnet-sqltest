d334cceb4599b0716196307fdb12de978643e926
Krzysztof Strugiński <cagrin@gmail.com>
2024-02-06 11:41:37 +0100

    Add second image to database tests

--- SqlTest.Tests/DatabaseTests/BaseDatabaseTests.cs
+++ SqlTest.Tests/DatabaseTests/BaseDatabaseTests.cs
@@ -5,7 +5,7 @@ public class BaseDatabaseTests
 {
     public static IEnumerable<object[]> Images => new[]
     {
-        // new object[] { "mcr.microsoft.com/mssql/server" },
+        new object[] { "mcr.microsoft.com/mssql/server" },
         new object[] { "mcr.microsoft.com/azure-sql-edge" },
     };
 
