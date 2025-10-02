62814f4ceed388e2b266e99362ae8d5d2a4fe661
Krzysztof Strugiński <cagrin@gmail.com>
2025-10-02 19:28:04 +0000

    Update packages

--- SqlTest.Tests/UnitTests/RunAllCommandTests.cs
+++ SqlTest.Tests/UnitTests/RunAllCommandTests.cs
@@ -24,14 +24,14 @@ Usage:
   SqlTest runall [options]
 
 Options:
-  -p, --project (REQUIRED)  Database project
-  -i, --image               Docker image
-  -c, --collation           Server collation
-  -r, --result              Save result to JUnit XML file
-  --cc-cobertura            Save code coverage to Cobertura XML file
-  --cc-disable              Disable code coverage
-  --cc-include-tsqlt        Include code coverage of tSQLt schema
-  -?, -h, --help            Show help and usage information
+  -p, --project <project> (REQUIRED)  Database project
+  -i, --image <image>                 Docker image
+  -c, --collation <collation>         Server collation
+  -r, --result <result>               Save result to JUnit XML file
+  --cc-cobertura <cc-cobertura>       Save code coverage to Cobertura XML file
+  --cc-disable                        Disable code coverage
+  --cc-include-tsqlt                  Include code coverage of tSQLt schema
+  -?, -h, --help                      Show help and usage information
 
 """;
 
